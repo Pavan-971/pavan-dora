e64b185ce5a899ed08b847cba73ed12f1d976ea9
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2021-06-24 15:34:33 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 dt="asok"
-json='{"STARTTIME": "'"$1"', "UNIQUEID": "'"$2"'","APIKEY": "'"$3"'"}'
+json='{"STARTTIME": "'"$1"', "UNIQUEID": "'"$2"'", "APIKEY": "'"$3"'", "SERVICEID": "'"$4"'"}'
 echo $json > pav.json
+curl -X POST "https://dora.aws-dev.jenkins.autodesk.com/v1/deploy-frequency" -H "accept: application/json" -H "Content-Type: application/json" -H "x-api-key: '$3'" -d "{\"serviceId\":\"'"$4"'\",\"deployTimestamp\":'"$1"',\"uniqueId\":\"'"$2"'\"}"
+
